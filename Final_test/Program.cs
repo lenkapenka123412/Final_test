9914ae8351484f8e62499e75f5be6eedbf1899a4
Elena <lenka.penka123412@gmail.com>
2023-06-07 02:36:59 +0300

    done diagram

--- Final_test/Program.cs
+++ Final_test/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿Console.Clear();
+Console.WriteLine("Введите массив строк через пробел: ");
+string[] mainArray = Console.ReadLine().Split(" ");
+string[] fullArray = new string[mainArray.Length];
+void InputArray(string[] mainArray, string[] fullArray)
+{
+    int x = 0;
+    for (int i = 0; i < mainArray.Length; i++)
+    {
+    if(mainArray[i].Length <= 3)
+        {
+        fullArray[x] = mainArray[i];
+        x++;
+        }
+    }
+}
+void PrintArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write($"{str[i]} ");
+    }
+    Console.WriteLine();
+}
+InputArray(mainArray, fullArray);
+Console.WriteLine("Вывод массива строк, состоящих из 3x и менее элементов: ");
+PrintArray(fullArray);
